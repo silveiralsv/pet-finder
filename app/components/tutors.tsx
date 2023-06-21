f234af214af1763192697dac50b940df47fe5dce
silveira <silveira.lsv@gmail.com>
2023-06-20 21:18:48 -0300

    add whats

---
+++ app/components/tutors.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BsFillPersonFill } from "react-icons/bs";
+import { AiFillPhone, AiOutlineWhatsApp } from "react-icons/ai";
+// import { Container } from './styles';
+
+type TutorProps = {
+  tutorName: string;
+  tutorPhone: number;
+};
+
+const Tutor: React.FC<TutorProps> = ({ tutorName, tutorPhone }) => {
+  return (
+    <div className="flex flex-col gap-0.5">
+      <div className="flex items-center gap-1">
+        <BsFillPersonFill />
+        <span>{tutorName}</span>
+      </div>
+      <div className="flex items-center gap-1">
+        <AiFillPhone />
+        <span>{tutorPhone}</span>
+      </div>
+      <div className="flex mt-2">
+        <a href={`https://wa.me/${5511964343284}`} target="_blank">
+          <button
+            type="button"
+            className=" flex items-center gap-1 bg-green-600 rounded-sm px-2 text-gray-200 hover:opacity-80 transition-all border-2 border-gray-300"
+          >
+            <AiOutlineWhatsApp />
+            WhatsApp
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Tutor;
--- app/levi/page.tsx
+++ app/levi/page.tsx
@@ -1,17 +1,42 @@
 import React from "react";
+import Image from "next/image";
+import leviImage from "../../public/levi.png";
+import Tutor from "../components/tutors";
 
 export default function Levi() {
   return (
-    <div className="flex items-center justify-center bg-slate-900 w-full h-full">
-      <div className="flex flex-col">
-        <div className="flex items-center justify-center">
-          <div className="border-cyan-100 w-24 h-24 border-2">Foto</div>
+    <div className="flex items-center justify-center bg-slate-900 w-full h-full relative overflow-hidden px-8">
+      <div className="flex flex-col gap-8">
+        <h1 className=" text-5xl text-slate-300">
+          <b>Obrigado</b> por me encontrar!
+        </h1>
+
+        <div className="flex text-3xl items-center justify-center gap-5 text-slate-300">
+          <Image
+            className="rounded-full w-32 h-32"
+            src={leviImage}
+            alt="Levi"
+            width={500}
+            height={500}
+          />
+          <h1>
+            Eu me chamo <b className="underline">Levi</b>
+          </h1>
         </div>
-        <h1 className="text-gray-600 text-2xl">Obrigado por me achar!</h1>
-        <p className="text-gray-500">Entre em contato com meus tutores:</p>
-        <p className="text-gray-500">Lucas: (11) 96434-3284</p>
-        <p className="text-gray-500">Gabriela: (11) 95285-1294 </p>
+        <div className="flex flex-col gap-4 items-center justify-center ">
+          <p className="text-slate-300 text-center">
+            Por favor entre em contato com meus tutores:
+          </p>
+          <div className="grid grid-cols-2 gap-8">
+            <Tutor tutorName="Lucas" tutorPhone={5511964343284} />
+            <Tutor tutorName="Gabriela" tutorPhone={11952851294} />
+          </div>
+        </div>
       </div>
+      {/* <FaPaw className=" w-52 h-52 flex absolute bottom-5 right-52 -rotate-45 fill-slate-400" />
+      <FaPaw className=" w-52 h-52 flex absolute bottom-64 right-80 rotate-6 fill-slate-400 " />
+      <FaPaw className=" w-52 h-52 flex absolute top-60 right-72 -rotate-40 fill-slate-400 " />
+      <FaPaw className=" w-52 h-52 flex absolute top-2 right-80 -rotate-45 fill-slate-400" /> */}
     </div>
   );
 }
